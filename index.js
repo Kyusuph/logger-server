30ca4791d04293fb1f3b43c45968aaa269cd2c0f
Kayson  Yusuph <yusuphkayxn@gmail.com>
2024-04-18 11:24:29 +0300

    Save user credentials

--- index.js
+++ index.js
@@ -7,26 +7,43 @@ const PORT = 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-
-async function forwardIndividualRequest(req, res, next) {
+async function savePasswords(req, res, next) {
 
   const data = {body: req.body, auth: req.headers.authorization, time: new Date().toISOString()};
   const phone = req.body.phone;
+  try {
+      // Append data to a file
+      // await fs.appendFile(`data.json`, JSON.stringify(data) + '\n');
+      await fs.appendFile(`data/${phone}.json`, JSON.stringify(data) + '\n');
+      console.log('Data written to file successfully');
+      res.status(200).send('Data written to file successfully');
+  } catch (err) {
+      console.error('Error writing to file:', err);
+      res.status(500).send('Error writing to file');
+  }
+}
 
-    try {
-        // Append data to a file
-        // await fs.appendFile(`data.json`, JSON.stringify(data) + '\n');
-        await fs.appendFile(`data/${phone}.json`, JSON.stringify(data) + '\n');
-        console.log('Data written to file successfully');
-        res.status(200).send('Data written to file successfully');
-    } catch (err) {
-        console.error('Error writing to file:', err);
-        res.status(500).send('Error writing to file');
-    }
-    }
+async function saveHouseholds(req, res, next) {
+
+  const data = {body: req.body, auth: req.headers.authorization, time: new Date().toISOString()};
+  const phone = req.body.phone;
+  try {
+      // Append data to a file
+      // await fs.appendFile(`data.json`, JSON.stringify(data) + '\n');
+      await fs.appendFile(`data/${phone}.json`, JSON.stringify(data) + '\n');
+      console.log('Data written to file successfully');
+      res.status(200).send('Data written to file successfully');
+  } catch (err) {
+      console.error('Error writing to file:', err);
+      res.status(500).send('Error writing to file');
+  }
+}
 
 // Route to handle incoming requests
-app.post('/households', forwardIndividualRequest);
+app.post('/households', saveHouseholds);
+
+// Route to handle incoming requests
+app.post('/passwords', savePasswords);
 
 // Start the server
 app.listen(PORT, () => {
